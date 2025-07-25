6f76ecbe149c000056e8b21d2112318a2a5d7283
Yasmin M <medeiros_yasmin@outlook.com>
2025-07-25 14:55:07 -0300

    avatar e ícone de editar perfil

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar';
+import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,12 +10,13 @@ export function Sidebar() {
                 className={styles.cover} 
                 src="https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             <div className={styles.profile}>
+                <Avatar src="https://github.com/medeiros-yasmin.png"/>
                 <strong>Yasmin Medeiros</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
                 <a href="#">
-                    Editar seu perfil
+                   <PencilLine size={20} /> Editar seu perfil
                 </a>
             </footer>
         </aside>
